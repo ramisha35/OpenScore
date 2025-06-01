29dff0ae9a4d3200922e1e7398a3ac2efc6185bc
ramisha35 <trami21@freeuni.edu.ge>
2025-06-01 16:30:26 +0400

    barrel pattern configuration in analyzer folder

---
+++ src/analyzers/index.ts
@@ -0,0 +1,7 @@
+export { schemaAndTypesAnalyzer } from './schemaAndTypesAnalyzers';
+export { descriptionsAndDocumentationAnalyzer } from './descriptionsAndDocumentationAnalyzer';
+export { pathsAndOperationsAnalyzer } from './pathsAndOperationsAnalyzer';
+export { responseCodesAnalyzer } from './responseCodesAnalyzer';
+export { examplesAndSamplesAnalyzer } from './examplesAndSamplesAnalyzer';
+export { securityAnalyzer } from './securityAnalyzer';
+export { miscellaneousBestPracticesAnalyzer } from './miscellaneousBestPracticesAnalyzer';
